a390cf7e71b689e712a170aea91cc3197f5e390a
Alexander Nigay <nigai.alex@gmail.com>
2013-11-10 22:21:33 +0600

    Enhanced existing code

--- mcmg/mcmg.py
+++ mcmg/mcmg.py
@@ -15,23 +15,24 @@ part = root.find('part')
 part_id = part.get('id')
 part_name = root.find("./part-list/score-part[@id='%s']" % part_id).find('part-name').text
 
-PRINT_NOTES = 15
+
+PRINT_NOTES = 35
 print 'First %d notes of part "%s":' % (PRINT_NOTES, part_name)
 for note in part.iter('note'):
 	step = note.find('pitch').find('step').text
 	octave = note.find('pitch').find('octave').text
 	type = note.find('type').text
-	
 	alter = note.find('pitch').find('alter')
+
+	halfsteps = 0
+	if (alter != None): halfsteps = int(alter.text)
+
 	accidental = None
-	if (alter == None or int(alter.text) == 0): 
-		accidental = ''
-	elif (int(alter.text) == 1):
-		accidental = '#'
-	elif (int(alter.text) == -1):
-		accidental = 'b'
+	if (halfsteps >= 0):
+		accidental = '#' * halfsteps
 	else:
-		accidental = '(alter value >1 or <-1)'
+		accidental = 'b' * halfsteps
+		
 
 	print step + accidental + octave + ' ' + type
 	
