80c9cf626241207b294b9bfa3b03c22a0c56be33
Alexander Nigay <nigai.alex@gmail.com>
2013-11-13 22:48:19 +0600

    Четко определил интерфейс цепи Маркова и сверил transition matrix с примером в книге

--- mcmg/mcmg.py
+++ mcmg/mcmg.py
@@ -7,49 +7,90 @@ import random
 
 class MarkovChain:
 	def __init__(self):
-		self.transition_matrix = {}
-		self.training_current_state = None
-		self.creation_current_state = None
+		self.transmat = {}
+		self.training_state = None
+		self.production_state = None
 
-	def consume(self, state):
-		prev_state = self.training_current_state
+	def _consume(self, state):
+		prev_state = self.training_state
 		if (prev_state == None): prev_state = '_None'
 
-		self.training_current_state = state
+		if (prev_state not in self.transmat.keys()):
+			self.transmat[prev_state] = {'_rowsum' : 0}
 
-		if (prev_state not in self.transition_matrix.keys()):
-			self.transition_matrix[prev_state] = {'_rowsum' : 0}
-
-		if (state not in self.transition_matrix[prev_state].keys()):
-			self.transition_matrix[prev_state][state] = 0
+		if (state not in self.transmat[prev_state].keys()):
+			self.transmat[prev_state][state] = 0
 		
-		self.transition_matrix[prev_state][state] += 1
-		self.transition_matrix[prev_state]['_rowsum'] += 1
+		self.transmat[prev_state][state] += 1
+		self.transmat[prev_state]['_rowsum'] += 1
+
+		if (state == '\0'):
+			self._reset_training()
+		else:
+			self.training_state = state
 
 
-	def produce(self):
-		prev_state = self.creation_current_state
+	def _produce(self):
+		prev_state = self.production_state
 		if (prev_state == None): prev_state = '_None'
 
-		roulette = random.randint(1, self.transition_matrix[prev_state]['_rowsum'])
+		roulette = random.randint(1, self.transmat[prev_state]['_rowsum'])
 		running_sum = 0
-		produced_state = None
-		for key in self.transition_matrix[prev_state]:
-			if (key == '_rowsum'): 
+		generated_state = None
+		for next_state in self.transmat[prev_state]:
+			if (next_state == '_rowsum'): 
 				continue
-			running_sum += self.transition_matrix[prev_state][key]
+			running_sum += self.transmat[prev_state][next_state]
 			if (running_sum >= roulette):
-				produced_state = key
+				generated_state = next_state
 				break
 	
-		self.creation_current_state = produced_state
-		return produced_state
+		if (generated_state == '\0'):
+			self._reset_production()
+		else:
+			self.production_state = generated_state
 
-	def reset_produce(self):
-		self.creation_current_state = None
+		return generated_state
 
-	def reset_consume(self):
-		self.training_current_state = None
+	def _reset_training(self):
+		self.training_state = None
+
+	def _reset_production(self):
+		self.production_state = None
+
+	def train(self, sequence):
+		if (len(sequence) == 0): return
+		if (self.training_state != None): raise RuntimeError('Markov chain is not in empty training state. Other training in progress or previous ended incorrectly')
+		for event in sequence:
+			self._consume(event)
+		self._consume('\0')
+
+	def generate(self, length_limit = 200):
+		sequence = []
+		counter = 0
+		while (counter < length_limit):
+			generated_state = self._produce()
+			if (generated_state == '\0'):
+				break
+			sequence.append(generated_state)
+			counter += 1
+
+		if (counter == length_limit):
+			self._reset_production()
+
+		return sequence
+	
+	def print_transmat(self):
+		def _escape_zero(v): 
+			if (v == '\0'): return '\\0' 
+			else: return v
+
+		for prev_state in self.transmat.keys():
+			str = prev_state + ': ['
+			for next_state in self.transmat[prev_state].keys():
+				str += '%s:%d, ' % (_escape_zero(next_state), self.transmat[prev_state][next_state])
+			str += ']'
+			print str
 
 import sys
 music_xml_filename = 'D:\Projects\MCMG\MusicXML\The_dance_of_victory-Eluveitie\lg-155582393382959147.xml'
@@ -90,7 +131,17 @@ for note in part.iter('note'):
 
 
 c = MarkovChain()
-words = ['hello', 'world', 'printer', 'mate', 'tame']
+words = '''
+mast tame same teams
+team meat steam stem
+'''.replace('\n', ' ').split(' ')
 for word in words:
-	map(c.consume, word+'0')
-	c.reset_consume()
+	c.train(word)
+c.print_transmat()
+
+
+print ''.join(c.generate())
+print ''.join(c.generate())
+print ''.join(c.generate())
+print ''.join(c.generate())
+print ''.join(c.generate())
